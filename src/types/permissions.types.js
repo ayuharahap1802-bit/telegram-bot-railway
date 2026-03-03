882e6962d4e29c415de1909d1ad13654a79c9c00
ayuharahap1802-bit <ayuharahap1802@gmail.com>
2026-03-03 14:05:34 +0700

    Create permissions.types.js

---
+++ src/types/permissions.types.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {Object} Permission
+ * @property {string} MANAGE_SYSTEM - Manage system settings
+ * @property {string} VIEW_LOGS - View system logs
+ * @property {string} MANAGE_ADMINS - Manage administrators
+ * @property {string} VIEW_STATS - View statistics
+ * @property {string} CREATE_PROMO - Create promotions
+ * @property {string} SEND_BROADCAST - Send broadcasts
+ * @property {string} SCHEDULE_PROMO - Schedule promotions
+ * @property {string} VIEW_PROMO - View promotions
+ * @property {string} CLAIM_BONUS - Claim bonuses
+ * @property {string} BAN_USER - Ban users
+ * @property {string} VIEW_ABUSE - View abuse reports
+ */
+
+module.exports = {};
