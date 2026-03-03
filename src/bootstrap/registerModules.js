3b0c27ab99ff7730476583b5dde9b4ee5f86fc95
ayuharahap1802-bit <ayuharahap1802@gmail.com>
2026-03-03 14:13:29 +0700

    Update registerModules.js

--- src/bootstrap/registerModules.js
+++ src/bootstrap/registerModules.js
@@ -8,20 +8,27 @@ async function registerModules(app) {
   const { bot, db, config } = app;
   
   try {
+    logger.info('Registering modules...');
+    
     // Setup all middlewares
-    setupMiddlewares(bot, db, config);
+    await setupMiddlewares(bot, db, config);
+    logger.info('Middlewares registered');
     
     // Register all commands
     await setupCommands(bot, db, config);
+    logger.info('Commands registered');
     
     // Start schedulers
     setupSchedulers(app);
+    logger.info('Schedulers started');
     
     // Start monitoring
     setupMonitoring(app);
+    logger.info('Monitoring started');
     
     // Launch bot
     await bot.launch();
+    logger.info('Bot launched');
     
     logger.info('All modules registered successfully');
     
