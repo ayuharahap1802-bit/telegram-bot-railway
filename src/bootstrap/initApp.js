32c49505c1bb17997db2e2a88d4a380721cea1f8
ayuharahap1802-bit <ayuharahap1802@gmail.com>
2026-03-03 14:13:12 +0700

    Update initApp.js

--- src/bootstrap/initApp.js
+++ src/bootstrap/initApp.js
@@ -1,26 +1,35 @@
 const { createBot } = require('../core/bot.factory');
-const { setupDatabase } = require('../database/connection');
+const { setupDatabase, getDatabase } = require('../database/connection');
 const { loadConfig } = require('../config/env.config');
 const logger = require('../shared/logger');
 
 async function initApp() {
   try {
+    logger.info('Initializing application...');
+    
     // Load configuration
     const config = loadConfig();
+    logger.info('Configuration loaded');
     
     // Setup database
     const db = await setupDatabase();
+    logger.info('Database connected');
     
     // Create bot instance
     const bot = createBot(config);
+    logger.info('Bot created');
     
     // Setup context extension
     require('../core/context.extension')(bot, db, config);
+    logger.info('Context extended');
     
     return { bot, db, config };
     
   } catch (error) {
-    logger.error('Init failed:', error);
+    logger.error('Init failed:', {
+      message: error.message,
+      stack: error.stack
+    });
     throw error;
   }
 }
