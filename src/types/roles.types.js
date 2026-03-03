64e114ba7bd685b59fb25dbbfc893943f95920ac
ayuharahap1802-bit <ayuharahap1802@gmail.com>
2026-03-03 14:05:20 +0700

    Create roles.types.js

---
+++ src/types/roles.types.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} Role
+ * @property {string} SUPER_ADMIN - Super administrator role
+ * @property {string} ADMIN - Administrator role
+ * @property {string} USER - Regular user role
+ * @property {string} BANNED - Banned user role
+ */
+
+/**
+ * @typedef {Object} RoleHierarchy
+ * @property {number} super_admin - Level 3
+ * @property {number} admin - Level 2
+ * @property {number} user - Level 1
+ * @property {number} banned - Level 0
+ */
+
+module.exports = {};
